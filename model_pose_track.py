2a23dc27802c8a59b077ea4757851bdf7e9c5abe
1 <user454e@>
2026-04-08 17:38:56 +0600

    dataset(maybe)

--- model_pose_track.py
+++ model_pose_track.py
@@ -1,3 +1,19 @@
+
+'''
 import kagglehub
 path = kagglehub.dataset_download("kmader/cmu-mocap")
-print("Path to dataset files:", path) #C:\Users\1\.cache\kagglehub\datasets\kmader\cmu-mocap\versions\4
+'''
+#print("Path to dataset files:", path) #C:\Users\1\.cache\kagglehub\datasets\kmader\cmu-mocap\versions\4
+
+
+
+# Скачайте с Roboflow (есть готовые bounding boxes):
+
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="YOUR_API_KEY")  # бесплатно на roboflow.com
+project = rf.workspace().project("ping-pong-ball-detection-sszkb")
+dataset = project.version(1).download("yolov8")
+
+
+
